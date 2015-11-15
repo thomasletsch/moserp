19193651aca2175b2a1cf44c5794592e685637ad
Thomas Letsch <contact@thomas-letsch.de>
2015-11-15 14:07:00 +0100

    Backend: Configured LDAP authentication and authorisation

--- backend/common/common-base/src/main/java/org/moserp/common/repository/EntityRepository.java
+++ backend/common/common-base/src/main/java/org/moserp/common/repository/EntityRepository.java
@@ -1,10 +1,12 @@
 package org.moserp.common.repository;
 
-import java.io.Serializable;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.security.access.prepost.PreAuthorize;
 
+import java.io.Serializable;
+
+@PreAuthorize("hasRole('user')")
 @NoRepositoryBean
 public interface EntityRepository<T, ID extends Serializable> extends CrudRepository<T, ID> {
 }
