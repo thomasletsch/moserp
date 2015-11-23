c0d08d59dffae7f4ea2a7d62e6e3e1ab73b818c2
Thomas Letsch <contact@thomas-letsch.de>
2015-11-23 13:34:59 +0100

    Backend: Added sales and order domains

---
+++ backend/order-module/order/src/main/java/org/moserp/order/repository/CustomerOrderRepository.java
@@ -0,0 +1,8 @@
+package org.moserp.order.repository;
+
+import org.moserp.common.repository.EntityRepository;
+import org.moserp.customer.domain.CustomerOrder;
+
+public interface CustomerOrderRepository extends EntityRepository<CustomerOrder, String> {
+
+}
---
+++ backend/sales-module/sales/src/main/java/org/moserp/sales/repository/SaleRepository.java
@@ -0,0 +1,8 @@
+package org.moserp.sales.repository;
+
+import org.moserp.common.repository.EntityRepository;
+import org.moserp.sales.domain.Sale;
+
+public interface SaleRepository extends EntityRepository<Sale, String> {
+
+}
