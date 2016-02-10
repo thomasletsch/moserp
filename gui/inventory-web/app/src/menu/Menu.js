bb18442c39f8100339236941873e92293673d223
Thomas Letsch <contact@thomas-letsch.de>
2016-02-10 16:35:12 +0100

    WEB: Small refactorings

--- gui/inventory-web/app/src/menu/Menu.js
+++ gui/inventory-web/app/src/menu/Menu.js
@@ -3,6 +3,12 @@ import MenuController from 'menu/MenuController'
 let moduleName = angular
     .module("menu", [])
     .controller("MenuController", MenuController)
+    .directive('appMenu', function () {
+        return {
+            templateUrl: 'src/menu/view/menu.html',
+            controller: MenuController
+        };
+    })
     .name;
 
 export default moduleName;
